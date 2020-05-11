62e2d7bb3c26ba8240b9cb57043540244898b791
lottemakinen <lottemaria99@gmail.com>
2020-05-11 20:50:53 +0300

    final version

--- work_hour_counter.py
+++ work_hour_counter.py
@@ -1,8 +1,10 @@
 def main():	
 	hours_list = []
+	counter = 1
 	weeks = int(input("How many weeks you have been working? "))
 	for n in range(weeks):
-		hours = int(input("Enter your work hours of the week: "))
+		hours = int(input("Enter your work hours of the week " + str(counter) + ": "))
+		counter = counter + 1
 		hours_list.append(hours)
 	print("Hours worked: ", sum(hours_list))
 main()
